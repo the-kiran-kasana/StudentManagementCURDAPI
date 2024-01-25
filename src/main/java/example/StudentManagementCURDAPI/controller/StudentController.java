89bb46407f612d137dafdb153231f846e1eb0901
kkasana <kkasanacoder@gmail.com>
2024-01-25 12:42:28 +0530

    made a controller that control the all RESTAPI

--- src/main/java/example/StudentManagementCURDAPI/controller/StudentController.java
+++ src/main/java/example/StudentManagementCURDAPI/controller/StudentController.java
@@ -1,5 +1,51 @@
 package example.StudentManagementCURDAPI.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import example.StudentManagementCURDAPI.model.Student;
+import example.StudentManagementCURDAPI.service.StudentService;
+
+
+@RestController
+@RequestMapping("/api/students")
 public class StudentController {
-    
+
+
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.createStudent(student);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+    }
+
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable Long id, @RequestBody Student updatedStudent) {
+        return studentService.updateStudent(id, updatedStudent);
+    }
 }
