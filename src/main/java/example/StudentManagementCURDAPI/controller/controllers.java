c5f2f8befdd7f2b7ffc3bed25e9c7cebfd5be26e
kkasana <kkasanacoder@gmail.com>
2024-01-25 15:38:36 +0530

    fatch the all data from databases to UI

--- src/main/java/example/StudentManagementCURDAPI/controller/controllers.java
+++ src/main/java/example/StudentManagementCURDAPI/controller/controllers.java
@@ -1,7 +1,9 @@
 package example.StudentManagementCURDAPI.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class controllers {
 
      /**
--- src/main/java/example/StudentManagementCURDAPI/service/StudentService.java
+++ src/main/java/example/StudentManagementCURDAPI/service/StudentService.java
@@ -31,7 +31,6 @@ public class StudentService {
     }
 
     public Student updateStudent(Long id, Student updatedStudent) {
-        // Implement update logic based on your requirements
         return studentRepository.save(updatedStudent);
     }
 }
