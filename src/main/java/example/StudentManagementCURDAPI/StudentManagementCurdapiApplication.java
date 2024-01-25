ca8a4a61b952ae4aac80e9d958942e4f04412fc8
kkasana <kkasanacoder@gmail.com>
2024-01-25 12:24:35 +0530

    add that file StudentManagementCURDAPI

---
+++ src/main/java/example/StudentManagementCURDAPI/StudentManagementCurdapiApplication.java
@@ -0,0 +1,13 @@
+package example.StudentManagementCURDAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementCurdapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementCurdapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/StudentManagementCURDAPI/StudentManagementCurdapiApplicationTests.java
@@ -0,0 +1,13 @@
+package example.StudentManagementCURDAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementCurdapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
